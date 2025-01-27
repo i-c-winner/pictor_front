6e969725844cc0d451be9a55d40434224c7226c9
Belousov Dmitriy <d-belousov@hotmail.com>
2025-01-27 17:35:37 +0500

    feat: добавит компонент карусель

--- src/pages/About/module/About.tsx
+++ src/pages/About/module/About.tsx
@@ -1,21 +1,54 @@
 import {Spotlight} from "../../../entities/spotLight/module/SpotLight.tsx";
 import {Box} from "@mui/material";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 
 function About() {
   if (import.meta.env.MODE === "development") {
     console.log("development");
   }
-
+  const responsive = {
+    desktop: {
+      breakpoint: {max: 3000, min: 1024},
+      items: 3,
+      slidesToSlide: 3 // optional, default to 1.
+    },
+    tablet: {
+      breakpoint: {max: 1024, min: 464},
+      items: 2,
+      slidesToSlide: 2 // optional, default to 1.
+    },
+    mobile: {
+      breakpoint: {max: 464, min: 0},
+      items: 1,
+      slidesToSlide: 1 // optional, default to 1.
+    }
+  };
   return (
-    <Box sx={{width: '100vw', height: '100vh', position: 'relative'}}>
-      <h1>About</h1>
-
-      <Spotlight />
-      <button style={{position: "relative"}} onClick={() => console.info('OK')}>console.</button>
-
-
-
+    <Box className="about">
+      <Carousel
+        containerClass="carousel"
+        swipeable={false}
+        draggable={false}
+        showDots={true}
+        responsive={responsive}
+        infinite={true}
+        autoPlay={true}
+        autoPlaySpeed={1000}
+        keyBoardControl={true}
+        customTransition="all .5"
+        transitionDuration={500}
+        removeArrowOnDeviceType={["tablet", "mobile"]}
+        dotListClass="custom-dot-list-style"
+        itemClass="carousel-item-padding-40-px"
+      >
+        <div>Item 1</div>
+        <div>Item 2</div>
+        <div>Item 3</div>
+        <div>Item 4</div>
+      </Carousel>
+      <Spotlight/>
     </Box>
   )
 }
