fcbf3211a930804926a2fe7da9c8cd318fc93663
Belousov Dmitriy <d-belousov@hotmail.com>
2025-01-28 10:24:50 +0500

    feat: создаст карусель для главных картинок

--- src/pages/About/module/About.tsx
+++ src/pages/About/module/About.tsx
@@ -1,53 +1,23 @@
 import {Spotlight} from "../../../entities/spotLight/module/SpotLight.tsx";
 import {Box} from "@mui/material";
-import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-
 
 function About() {
   if (import.meta.env.MODE === "development") {
     console.log("development");
   }
-  const responsive = {
-    desktop: {
-      breakpoint: {max: 3000, min: 1024},
-      items: 3,
-      slidesToSlide: 3 // optional, default to 1.
-    },
-    tablet: {
-      breakpoint: {max: 1024, min: 464},
-      items: 2,
-      slidesToSlide: 2 // optional, default to 1.
-    },
-    mobile: {
-      breakpoint: {max: 464, min: 0},
-      items: 1,
-      slidesToSlide: 1 // optional, default to 1.
-    }
-  };
+
   return (
     <Box className="about">
-      <Carousel
-        containerClass="carousel"
-        swipeable={false}
-        draggable={false}
-        showDots={true}
-        responsive={responsive}
-        infinite={true}
-        autoPlay={true}
-        autoPlaySpeed={1000}
-        keyBoardControl={true}
-        customTransition="all .5"
-        transitionDuration={500}
-        removeArrowOnDeviceType={["tablet", "mobile"]}
-        dotListClass="custom-dot-list-style"
-        itemClass="carousel-item-padding-40-px"
+      <div
+        className="carousel"
       >
-        <div>Item 1</div>
-        <div>Item 2</div>
-        <div>Item 3</div>
-        <div>Item 4</div>
-      </Carousel>
+        <div className={"img"}><img className={'img img_1'} src="../../../../public/images/carousel/1712837652574.jpeg"/></div>
+        <div className={"img"}><img className="img img_2" src="../../../../public/images/carousel/222683593504.jpg.webp"/></div>
+        <div className={"img"}><img className={"img img_3"} src="../../../../public/images/carousel/u26g8z4dynglriin2ko9cbccy8fy4g8s.jpg.webp"/></div>
+        <div className={"img"}><img className={"img img_4"} src="../../../../public/images/carousel/tanos.jpg"/></div>
+        <div className={"img"}><img className={"img img_5"} src="../../../../public/images/carousel/cat.jpg"/></div>
+      </div>
       <Spotlight/>
     </Box>
   )
