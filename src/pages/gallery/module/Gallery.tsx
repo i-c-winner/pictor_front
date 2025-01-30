fb58822b90caec9421ee2c80de938b368742fdd3
Belousov Dmitriy <d-belousov@hotmail.com>
2025-01-30 12:11:36 +0500

    feat: создаст poxyserver

--- src/pages/gallery/module/Gallery.tsx
+++ src/pages/gallery/module/Gallery.tsx
@@ -1,6 +1,27 @@
+import useSwr from 'swr'
 function Gallery() {
+  const fetching=(url: string)=>fetch(url, {
+    method:'GET',
+    headers:{
+      'Accept': 'application/json',
+    }
+  }).then(res=>res)
+  const {data, isLoading, error} = useSwr(`${import.meta.env.VITE_PATH_TO_BACKAND}backand/files/addfiles.php`, fetching);
+  if(isLoading) {
+    return <div style={{color: "white"}}> Loading...</div>;
+  }
+  if (data) {
+    console.info(data, 'DATA')
+    return <div style={{color: "white"}}>Загрузили</div>;
+  }
+
+  if (error) {
+    console.log(data, error);
+    return <div style={{color: "red"}}> Error 404</div>;
+  }
   return (
-    <p>gallery</p>
+    <p style={{color: "white"}}>poop</p>
   )
+
 }
 export {Gallery};
